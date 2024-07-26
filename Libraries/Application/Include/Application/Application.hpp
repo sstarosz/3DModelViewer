bcde965b5d6b83d3d1e117bc1f6185ca5b8631d8
Sebastian Starosz <staroszsebastian@gmail.com>
2024-07-27 00:15:02 +0200

    chore: Add Modifier class and related files to Application

--- Libraries/Application/Include/Application/Application.hpp
+++ Libraries/Application/Include/Application/Application.hpp
@@ -5,80 +5,44 @@
 #include <spdlog/spdlog.h>
 #include <vector>
 
+#include <QApplication>
+#include "Core/CommandManager.hpp"
+#include "Core/ContentManager.hpp"
+#include "Application/Creator.hpp"
+#include "Application/Modifier.hpp"
+
+#include "Ui/GuiManager.hpp"
+
 namespace st::application
 {
-    
+
 	class Application
 	{
 	  public:
-		Application(int argc, char* argv[]) :
-			m_app(argc, argv),
-			m_commandManager(),
-			m_contentManager(),
-			m_guiManager(core::ContentManagerHandler(&m_contentManager)),
-			m_creator(core::ContentManagerHandler(&m_contentManager), CommandManagerHandler(&m_commandManager))
-		{
-			spdlog::set_level(spdlog::level::debug);
-			spdlog::info("Application::Application()");
-			spdlog::info("Application::Application() - Done");
-			spdlog::info("----------------------");
-		}
+		Application(int argc, char* argv[]);
 
 
-		int initialize()
-		{
-			spdlog::info("Application::initialize()");
-			m_contentManager.initialize();
-			m_commandManager.initialize();
-			m_guiManager.initialize();
+		int initialize();
 
-			spdlog::info("Application::initialize() - Done");
-			spdlog::info("----------------------");
-			return 0;
-		}
+		int run();
 
+		Creator& create();
 
-		int run()
-		{	
-			spdlog::info("Application::run()");	
-
-			m_contentManager.onStart();
-			
-			m_guiManager.show();
-
-			return m_app.exec();
-		}
-
-		Creator& create()
-		{
-			return m_creator;
-		}
-
-		Modifier modify(std::weak_ptr<core::Node> node)
-		{
-			ModifyContext context;
-            context.m_contentManager = &m_contentManager;
-			context.selectedNode = node;
-
-
-			Modifier modifier{context, CommandManagerHandler(&m_commandManager)};
-			return modifier;
-		}
+		Modifier modify(std::weak_ptr<core::Node> node);
 
 
 	  private:
 		QApplication m_app;
 
 		//Communication withing the application
-		CommandManager m_commandManager;
-
+		core::CommandManager m_commandManager;
+		core::ContentManager m_contentManager;
 
 		// Content
-		core::ContentManager m_contentManager;
 		Creator m_creator;
 
 		// Gui
-		GuiManager m_guiManager;
+		ui::GuiManager m_guiManager;
 
 		// GuiCreator m_guiCreator; To investigate future use
 	};
---
+++ Libraries/Application/Source/Modifier.cpp
@@ -0,0 +1,59 @@
+#include "Modifier.hpp"
+
+#include "Renderer/Commands/MaterialCommands.hpp"
+
+namespace st::application
+{
+
+	/*---------------------------------*/
+	/*--------MaterialModifier---------*/
+	/*---------------------------------*/
+	MaterialModifier::MaterialModifier(ModifyContext context, core::CommandManagerHandler commandManager) :
+		m_context(context),
+		m_commandManager(commandManager)
+	{
+	}
+
+	void MaterialModifier::assign(std::weak_ptr<renderer::StandardMaterial> material)
+	{
+		// TODO refactor
+		std::shared_ptr<renderer::StandardMaterial> materialPtr = material.lock();
+		std::shared_ptr<core::Node> nodePtr = m_context.selectedNode.lock();
+		std::unique_ptr<renderer::CommandAssignMaterial> command = std::make_unique<renderer::CommandAssignMaterial>(m_context.m_contentManager, materialPtr, nodePtr);
+		m_commandManager->execute(std::move(command));
+	}
+
+	/*---------------------------------*/
+	/*------------Modifier-------------*/
+	/*---------------------------------*/
+
+	Modifier::Modifier(ModifyContext context,
+					   core::CommandManagerHandler commandManager) :
+		m_context(context),
+		m_commandManager(commandManager)
+	{
+	}
+
+	void Modifier::transform()
+	{
+		// TODO refactor
+	}
+
+	MaterialModifier Modifier::material()
+	{
+		// TODO refactor
+		//  Throw exception if node can't have material
+		if (auto node = m_context.selectedNode.lock())
+		{
+			// Throw if node is not of type plane
+			if (!std::dynamic_pointer_cast<geometry::Plane>(node))
+			{
+				throw std::runtime_error("Node is not of type Plane");
+			}
+		}
+
+		MaterialModifier materialManager{m_context, m_commandManager};
+		return materialManager;
+	}
+
+} // namespace st::application
